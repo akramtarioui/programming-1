6ac108d86a421325712c38c0bfa659e281c8743f
Akram Tarioui <517379@student.glu.nl>
2018-09-13 10:03:32 +0200

    Opdrachten.

---
+++ les 3/Lists.py
@@ -0,0 +1,5 @@
+favorieten = ['artiest-1']
+favorieten.append('artiest-2')
+favorieten[1] = 'artiest-nieuw'
+print(favorieten)
+
---
+++ les 3/numbers.py
@@ -0,0 +1,8 @@
+list = [ 3, 7, -2, 12,]
+
+a = max(list)
+b = min(list)
+
+print (a - b)
+
+print(max(list) - min(list))
